f8652cb34a73f80f3a0e54fbd925c22009ff399c
Ilayaperumal Gopinathan <iperumal@vmware.com>
2012-02-28 14:56:51 -0800

    Add UserStatistics JSON data into view

--- nanotrader-bb/js/model/accountsummary.js
+++ nanotrader-bb/js/model/accountsummary.js
@@ -1,17 +1,18 @@
 AccountSummary = Backbone.Model.extend({
 			initialize : function() {
 				var model = this;
-				var test = $.ajax({
-							type : "POST",
+				$.ajax({
+							type : "GET",
 							url : 'data/accountsummary.json',
 							dataType : 'json',
 							//Make synchronous ajax call
 							async:   false,
+							cache: false,
 							success : function(response) {
 								model.set(response.results[0]);
 							},
 							error : function(xhr, textStatus, errorThrown) {
-								alert(xhr.status);
+								alert('Error'+ xhr.status + " " + errorThrown);
 							}
 						});
 			}
--- nanotrader-bb/js/model/userstatistics.js
+++ nanotrader-bb/js/model/userstatistics.js
@@ -1,10 +1,19 @@
 UserStatistics = Backbone.Model.extend({
-			defaults : {
-				'summary_head': 'User Statistics',
-				'account_id' : 1,
-				'account_creation_date' : '1/1/2012',
-				'total_logins' : 10,
-				'session_creation_date' : '2/25/2012'
-
+			initialize : function() {
+				var model = this;
+				$.ajax({
+							type : "POST",
+							url : 'data/userstatistics.json',
+							dataType : 'json',
+							// Make synchronous ajax call
+							async : false,
+							cache: false,
+							success : function(response) {
+								model.set(response.results[0]);
+							},
+							error : function(xhr, textStatus, errorThrown) {
+								alert('Error'+ xhr.status + " " + errorThrown);
+							}
+						});
 			}
 		});
