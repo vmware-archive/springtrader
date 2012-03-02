2ca372fc055e6a1194b677ffcc87ac3b502ed26f
dussab <dussab@verizon.net>
2012-03-02 15:00:26 -0500

    fixed build script. dependencies are being properly resolved now

--- spring-nanotrader-data/src/main/java/org/springframework/nanotrader/domain/Account.java
+++ spring-nanotrader-data/src/main/java/org/springframework/nanotrader/domain/Account.java
@@ -18,7 +18,6 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
-
 import org.springframework.format.annotation.DateTimeFormat;
 
 @SuppressWarnings("serial")
