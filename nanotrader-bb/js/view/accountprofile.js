bf3b10db4a66f083c784a139efadcdd525004385
Kashyap Parikh <parikhkc@gmail.com>
2012-03-05 16:55:50 -0800

    remove alert message

--- nanotrader-bb/js/view/accountprofile.js
+++ nanotrader-bb/js/view/accountprofile.js
@@ -10,7 +10,6 @@ window.AccountProfileView = Backbone.View.extend({
     render: function(myid) {
         //Backbone.Validation.bind(this);
         var ap = new AccountProfile({id: myid});
-        alert("my url is : " + ap.url());
         var type = ap.toJSON();
         $(this.el).html(this.template(ap.toJSON()));
         return this;
