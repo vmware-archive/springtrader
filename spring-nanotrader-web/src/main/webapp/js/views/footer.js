82455614168fe768daa16cab40186be510824622
Kashyap Parikh <parikhkc@gmail.com>
2012-04-20 16:45:23 -0700

    Add web project for static html. Add admin page for data refresh

---
+++ spring-nanotrader-web/src/main/webapp/js/views/footer.js
@@ -0,0 +1,51 @@
+/**
+ * View class for the Footer
+ * @author Carlos Soto <carlos.soto@lognllc.com>
+ */
+nano.views.Footer = Backbone.View.extend({
+    
+    /**
+     * Bind the events functions to the different HTML elements
+     */
+    events : {
+        'click #contactUsBtn' : 'contact',
+        'click #helpBtn' : 'help'
+    },
+    
+    /**
+     * Class constructor
+     * @author Carlos Soto <carlos.soto@lognllc.com>
+     * @param Object options:
+     * - el: selector for the container
+     * @return void
+     */
+    initialize : function(options) {
+        nano.containers.footer = this.$el;
+    },
+
+    /**
+     * Templating function (inyects data into an HTML Template)
+     * @author Carlos Soto <carlos.soto@lognllc.com>
+     * @param Object data: data to be replaced in the template
+     * @return string
+     */
+    template : _.template(nano.utils.getTemplate(nano.conf.tpls.footer)),
+
+    /**
+     * Renders the Account Summary View
+     * @author Carlos Soto <carlos.soto@lognllc.com>
+     * @return void
+     */
+    render: function() {
+            var footer = this.template();
+            this.$el.html(footer);
+    },
+    
+    contact: function() {
+        window.location = nano.conf.hash.contact;
+    },
+    
+    help: function() {
+        alert('goto -> help');
+    }
+});
