f17b6453b34eaba9731743548e3aeabda45f9115
Kashyap Parikh <parikhkc@gmail.com>
2012-04-25 12:37:50 -0700

    add option to pass app server host/port for generateData

--- tests/src/main/resources/NanotraderGenerateData.groovy
+++ tests/src/main/resources/NanotraderGenerateData.groovy
@@ -21,20 +21,34 @@ Example after populated at runtime:
    kashyap,passwd,e0480709-4063-416e-a3ce-361025d4fc61,101,ATVI:ADBE:AKAM:AMZN,2,500:501,100000.00
 
 */
+
+
 List userdata
+String host = "localhost"
+String port = "8080"
+String apiURL = "http://" + host + ":" + port
+
+def readArgs() {
+  if (args.length == 2) {
+    apiURL = "http://" + args[0] + ":" + args[1] 
+  } else if (args.length == 1)
+      apiURL = "http://" + args[0] + ":8080"
+  println "Populating data on : " + apiURL
+}
 
 def createQuotes() {
   // populate quote table if not done already
 }
 
 def registerUsers() {
-  def client = new NanoTraderRESTClient()
+  def client = new NanoTraderRESTClient(path: apiURL)
   client.createFirstUser()
   client.init()
   // read names.csv in to a List
   CSVReader reader = new CSVReader(new FileReader("names.csv"))
   userdata = reader.readAll();
   // register all users in userdata List
+  println "Creating users..."
   userdata.eachWithIndex() { obj, i ->
     def username = obj[0]
     def password = obj[1]
@@ -43,13 +57,13 @@ def registerUsers() {
     try {
       client.createAccountProfile(username, false, 201, password, openbal)
     } catch (ex) {
-      println "Failed to register user :: " + username + " (HTTP response = [" + ex.getMessage() + "])"
+      //println "Failed to register user :: " + username + " (HTTP response = [" + ex.getMessage() + "])"
     } 
   }
 }
 
 def getTokens() {
-  def client = new NanoTraderRESTClient()
+  def client = new NanoTraderRESTClient(path: apiURL)
   client.init()
   // get authToken and account id for users in userdata List
   // Note: Remember to call rgisterUsers() before calling this function
@@ -63,10 +77,11 @@ def getTokens() {
 }
 
 def buyOrders() {
+  println "Creating buy orders..."
   userdata.eachWithIndex() { obj, i ->
     int acctid = obj[3].toInteger()
     authToken = obj[2]
-    def client = new NanoTraderRESTClient()
+    def client = new NanoTraderRESTClient(path: apiURL)
     client.init()
     //read 4th column from names.cvs and split around ":" to get buy symbols
     def quoteArray = obj[4].split(":")
@@ -85,17 +100,20 @@ def buyOrders() {
 }
 
 def sellOrders() {
+  println "Creating sell orders..."
   userdata.eachWithIndex() { obj, i ->
+    println "For user -> " + obj[1]
     int acctid = obj[3].toInteger()
     def authToken = obj[2]
     def orderArray = obj[6].split(":")
-    def client = new NanoTraderRESTClient()
+    def client = new NanoTraderRESTClient(path: apiURL)
     client.init()
     //read 5th column from names.cvs to get count of sell orders
     int sellCount = obj[5].toInteger()
     //println " Sell order for : " + obj[0] + "  " + obj[6]
     sellCount.times { 
       // Call sell api
+      println "   Selling  -> " + it
       def orderdetail = client.getOrder(acctid, orderArray[it].toInteger(), authToken)
       def holdingid = new JsonSlurper().parseText(orderdetail).holdingid
       client.createSellOrder(acctid, holdingid, true, 201, authToken)
@@ -103,7 +121,10 @@ def sellOrders() {
   } 
 }
 
+readArgs()
 registerUsers()
 getTokens()
 buyOrders()
 sellOrders()
+
+
