6719f88310b97d3b4fcc6c7aacfbce7e7c81122d
Carlos Soto <carlos.soto@lognllc.com>
2012-04-30 16:32:12 -0600

    Fixing "this.username is undefined" error

--- nanotrader-web-logn/app/js/views/navbar.js
+++ nanotrader-web-logn/app/js/views/navbar.js
@@ -52,10 +52,11 @@ nano.views.Navbar = Backbone.View.extend({
         {
             var hash = nano.conf.hash.dashboard;
         }
-        this.$el.html(this.template(nano.session));
-        
+
         if ( !this.$el.html() )
         {
+            this.$el.html(this.template(nano.session));
+
             // Enable the dropdown for the User Profile options on the right
             this.$('.dropdown-toggle').dropdown(); 
 
