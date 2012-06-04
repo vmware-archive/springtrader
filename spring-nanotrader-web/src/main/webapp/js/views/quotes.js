514eb1e0d00a46bb6d9696f7b380c48d97be6c22
Ilayaperumal Gopinathan <iperumal@vmware.com>
2012-06-03 22:14:16 -0700

    Add modal pop up width as auto and relative

--- spring-nanotrader-web/src/main/webapp/js/views/quotes.js
+++ spring-nanotrader-web/src/main/webapp/js/views/quotes.js
@@ -109,7 +109,12 @@ nano.views.Quotes = Backbone.View.extend({
         var onSuccess = function(model){
            
             var popup = $( _.template(nano.utils.getTemplate(nano.conf.tpls.quoteModal))(model.toJSON()) );
-            popup.modal();
+            popup.modal().css({
+                width: 'auto',
+                'margin-left': function () {
+                    return -($(this).width() / 2);
+                }
+            });
             
             view.$el.empty();
             nano.instances.router.trade(view.page);
