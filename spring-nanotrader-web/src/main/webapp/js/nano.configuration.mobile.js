0453b60faca3aac6ee8dc4cfa8d7e7804a2dcf2c
Carlos Soto <carlos.soto@lognllc.com>
2012-11-28 12:24:11 -0600

    NTR-157: Nanotrader Login - Web Version | Login button in the Main page section is misaligned.
    https://issuetracker.springsource.com/browse/NTR-157

--- spring-nanotrader-web/src/main/webapp/js/nano.configuration.mobile.js
+++ spring-nanotrader-web/src/main/webapp/js/nano.configuration.mobile.js
@@ -8,7 +8,6 @@ _.extend(nano.conf.tpls, {
     marketSummary : nano.conf.tplRoot + 'marketSummary.mobile.tpl',
     portfolioSummary : nano.conf.tplRoot + 'portfolioSummary.mobile.tpl',
     accountSummary : nano.conf.tplRoot + 'accountSummary.mobile.tpl',
-    login : nano.conf.tplRoot + 'login.mobile.tpl',
     portfolio : nano.conf.tplRoot + 'portfolio.mobile.tpl',
     positions : nano.conf.tplRoot + 'positions.mobile.tpl',
     userStatistics : nano.conf.tplRoot + 'userStatistics.mobile.tpl',
