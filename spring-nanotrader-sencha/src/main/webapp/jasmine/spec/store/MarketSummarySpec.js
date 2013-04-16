b587dd17a334a609d95679ffeb58b2bd01436d05
Ilayaperumal Gopinathan and Ken Mayer <pair+ilaya+ken@vmware.com>
2013-04-16 14:06:20 -0700

    Add more tests for MarketSummary store
    - Assert that field values are properly set
    - Refactor fixture data
    
    [#46856245]

--- spring-nanotrader-sencha/src/main/webapp/jasmine/spec/store/MarketSummarySpec.js
+++ spring-nanotrader-sencha/src/main/webapp/jasmine/spec/store/MarketSummarySpec.js
@@ -1,8 +1,26 @@
 describe('SpringTrader.store.MarketSummary', function () {
+    var store, request;
+    beforeEach(function() {
+        store = Ext.create('SpringTrader.store.MarketSummary');
+        request = mostRecentAjaxRequest();
+    });
     it("calls out to the proper url", function () {
-        jasmine.Ajax.assertInstalled();
-        var store = Ext.create('SpringTrader.store.MarketSummary');
-        var request = mostRecentAjaxRequest();
         expect(request.url).toEqual('/spring-nanotrader-services/api/marketSummary');
+    });
+
+    it("assigns the proper attributes from the data", function() {
+        request.response({
+            status: 200,
+            responseText: marketSummaryJSON
+        });
+
+        expect(store.getCount()).toEqual(1);
+
+        var marketSummaryData = store.getAt(0);
+        expect(marketSummaryData.get('tradeStockIndexAverage')).toEqual(103.20);
+        expect(marketSummaryData.get('tradeStockIndexVolume')).toEqual(22497.00);
+        expect(marketSummaryData.get('change')).toEqual(-125.09);
+        expect(marketSummaryData.get('topLosers').length).toEqual(3);
+        expect(marketSummaryData.get('topGainers').length).toEqual(3);
     });
 });
