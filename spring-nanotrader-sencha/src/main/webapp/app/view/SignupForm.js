e7e3a8621f415cb6777e19417635cb7f3df30f7c
Ilayaperumal Gopinathan and Ken Mayer <pair+ilaya+ken@vmware.com>
2013-04-24 11:16:35 -0700

    Restyle sign up form
    [#46872821]

--- spring-nanotrader-sencha/src/main/webapp/app/view/SignupForm.js
+++ spring-nanotrader-sencha/src/main/webapp/app/view/SignupForm.js
@@ -3,17 +3,26 @@ Ext.define('SpringTrader.view.SignupForm', {
     xtype: 'signupPage',
     requires: ['Ext.field.Email', 'Ext.field.Password', 'Ext.field.Number'],
     config: {
-        title: 'Sign up Form',
+        title: 'Sign up',
         items: [
-            { xtype: 'textfield', name: 'fullname', placeHolder: 'Full Name'},
-            { xtype: 'emailfield', name: 'email', placeHolder: 'email@vmware.com' },
-            { xtype: 'passwordfield', name: 'passwd', placeHolder: 'Password'},
-            { xtype: 'passwordfield', name: 'passwdconfirm', placeHolder: 'Confirm Password'},
-            { xtype: 'textfield', name: 'userid', placeHolder: 'Username'},
-            { xtype: 'numberfield', name: 'openbalance', placeHolder: 'Opening balance', minValue: 100000, stepValue: 1000},
-            { xtype: 'numberfield', name: 'creditcard', value: '1234123412341234', disabled: true},
-            { xtype: 'textfield', name: 'address', placeHolder: 'Address' },
-            { xtype: 'button', ui: 'confirm', text: 'Submit', itemId: 'signupSubmitBtn'}
+            { xtype: 'fieldset',
+                items: [
+                    { xtype: 'textfield', name: 'fullname', placeHolder: 'Name' },
+                    { xtype: 'emailfield', name: 'email', placeHolder: 'Email address' },
+                    { xtype: 'passwordfield', name: 'passwd', placeHolder: 'Password'},
+                    { xtype: 'passwordfield', name: 'passwdconfirm', placeHolder: 'Confirm password'},
+                    { xtype: 'textfield', name: 'userid', placeHolder: 'Username'},
+                    { xtype: 'numberfield', name: 'openbalance', placeHolder: 'Opening balance', minValue: 100000, stepValue: 1000},
+                    { xtype: 'textfield', name: 'address', placeHolder: 'Address' },
+                    { xtype: 'numberfield', name: 'creditcard', placeHolder: 'CC# 1234 1234 1234 1234', disabled: true},
+                    { xtype: 'container',
+                        layout: { type: 'vbox', align: 'center', pack: 'center' },
+                        items: [
+                            { xtype: 'button', ui: 'confirm', text: 'Sign up', itemId: 'signupSubmitBtn' }
+                        ]
+                    }
+                ]
+            },
         ]
     }
 });
