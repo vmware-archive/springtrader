c77e956611519d65907b54cae1b14fa3a119de14
Ilayaperumal Gopinathan and Ken Mayer <pair+ilaya+ken@vmware.com>
2013-04-26 13:58:49 -0700

    Logged out view is scrollable
    - This bug is only viewable in real devices and simulators
    
    [#48824679]

--- spring-nanotrader-sencha/src/main/webapp/app/view/LoggedOut.js
+++ spring-nanotrader-sencha/src/main/webapp/app/view/LoggedOut.js
@@ -2,6 +2,8 @@ Ext.define('SpringTrader.view.LoggedOut', {
     extend: 'Ext.Container',
     xtype: 'loggedoutview',
     config: {
+        scrollable: true,
+        height: '100%',
         items: [
             { xtype: 'marketsummary' },
             { xtype: 'signupbutton' }]
