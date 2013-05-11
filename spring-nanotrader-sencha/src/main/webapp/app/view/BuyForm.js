dc7b63ae6652cfe764b600f3bc39af12af7f1331
Ilayaperumal Gopinathan and Ken Mayer <pair+ilaya+ken@vmware.com>
2013-05-10 17:10:43 -0700

    Trader can buy a stock
     - add buy POST request success/failure callbacks
     - add new number field number pattern updated
     - enable/disable buy button based on quantity field value
    
     [#48537931]

--- spring-nanotrader-sencha/src/main/webapp/app/view/BuyForm.js
+++ spring-nanotrader-sencha/src/main/webapp/app/view/BuyForm.js
@@ -1,21 +1,24 @@
 Ext.define('SpringTrader.view.BuyForm', {
     extend: 'Ext.form.Panel',
     xtype: 'buyform',
-    requires: ['Ext.field.Number', 'Ext.form.FieldSet'],
+    requires: ['SpringTrader.field.Number', 'Ext.form.FieldSet'],
     config: {
         height: '100%',
         width: '100%',
         items: [
             { xtype: 'fieldset',
                 items: [
-                    { xtype: 'numberfield', name: 'quantity', label: 'Quantity' }
+                    {
+                        xtype: 'stnumberfield',
+                        name: 'quantity',
+                        label: 'Quantity'
+                    }
                 ]
             },
             { xtype: 'container',
                 layout: { type: 'hbox', align: 'center', pack: 'center' },
                 items: [
-                    { xtype: 'button', ui: 'cancel', text: 'Cancel', itemId: 'cancelButton', margin: '0 10 0 10' },
-                    { xtype: 'button', ui: 'confirm', text: 'Buy', itemId: 'submitButton', margin: '0 10 0 10' }
+                    { xtype: 'button', disabled: true, ui: 'confirm', text: 'Buy', itemId: 'submitButton', margin: '0 10 0 10' }
                 ]
             }
         ]
