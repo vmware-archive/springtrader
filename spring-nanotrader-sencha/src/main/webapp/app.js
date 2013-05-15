d35d6ac4852f3f3028907e9422b4c1719b4ab6c7
Ilayaperumal Gopinathan and Ken Mayer <pair+ilaya+ken@vmware.com>
2013-05-15 14:50:07 -0700

    Install custom icon assets
    [#47847619]

--- spring-nanotrader-sencha/src/main/webapp/app.js
+++ spring-nanotrader-sencha/src/main/webapp/app.js
@@ -95,21 +95,21 @@ Ext.application({
 	views: ['Main'],
 
     icon: {
-        '57': 'resources/icons/Icon.png',
-        '72': 'resources/icons/Icon~ipad.png',
-        '114': 'resources/icons/Icon@2x.png',
-        '144': 'resources/icons/Icon~ipad@2x.png'
+        '57': 'resources/icons/icon_57.png',
+        '72': 'resources/icons/icon_72.png',
+        '114': 'resources/icons/icon_114.png',
+        '144': 'resources/icons/icon_144.png'
     },
 
-    isIconPrecomposed: true,
+    isIconPrecomposed: false,
 
     startupImage: {
-        '320x460': 'resources/startup/320x460.jpg',
-        '640x920': 'resources/startup/640x920.png',
-        '768x1004': 'resources/startup/768x1004.png',
-        '748x1024': 'resources/startup/748x1024.png',
-        '1536x2008': 'resources/startup/1536x2008.png',
-        '1496x2048': 'resources/startup/1496x2048.png'
+        '320x460': 'resources/startup/light/320x460.png',
+        '640x920': 'resources/startup/light/640x920.png',
+        '768x1004': 'resources/startup/light/768x1004.png',
+        '748x1024': 'resources/startup/light/748x1024.png',
+        '1536x2008': 'resources/startup/light/1536x2008.png',
+        '1496x2048': 'resources/startup/light/1496x2048.png'
     },
 
     launch: function() {
